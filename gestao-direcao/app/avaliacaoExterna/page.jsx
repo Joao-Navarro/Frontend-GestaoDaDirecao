d265ebea7e8338a5c58b5926a9cf6740520d5f9f
Pietrasouza20 <pietrasouza2504@gmail.com>
2024-09-24 08:43:05 -0300

    atualização

--- gestao-direcao/app/avaliacaoExterna/page.jsx
+++ gestao-direcao/app/avaliacaoExterna/page.jsx
@@ -1,20 +1,18 @@
-
 "use client"
 import { useState } from 'react';
-import Footer from '@/components/Footer';
+import style from "./page.module.css"
 import Header from '@/components/Header';
-import styles from './page.module.css';
+import Footer from '@/components/Footer';
 
-
-
-
+<header/>
 const studentsData = [
-  { aluno: 'João', rm: '123' },
+  { aluno: 'João', rm: '123', },
   { aluno: 'Maria', rm: '456',},
-  { aluno: 'Pedro', rm: '789' },
-  { aluno: 'Ana', rm: '101'},
-  { aluno: 'Lucas', rm: '202',}
+  { aluno: 'Pedro', rm: '789',},
+  { aluno: 'Ana', rm: '101',},
+  { aluno: 'Lucas', rm: '202',},
 ];
+
 const Home = () => {
   const [filter, setFilter] = useState({
     ensino: '',
@@ -22,9 +20,7 @@ const Home = () => {
     ano: '',
   });
 
-
   const [filteredStudents, setFilteredStudents] = useState(studentsData);
-
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
@@ -33,7 +29,6 @@ const Home = () => {
       [name]: value,
     }));
   };
-
 
   const handleFilter = () => {
     // Simulação de filtro baseado nos valores. Adapte conforme necessário.
@@ -45,122 +40,107 @@ const Home = () => {
     setFilteredStudents(newFilteredStudents);
   };
 
-
-
   return (
-
     <>
 
+        <Header/>
 
+      <div className={style.filtro}>
+        <label>
+          <select className={style.button} name="ensino" onChange={handleFilterChange} value={filter.ensino}>
+            <option value="fundamental1">Ensino Fundamental 1</option>
+            <option value2="fundamental1">1°</option>
+            <option value3="fundamental1">2°</option>
+            <option value4="fundamental1">3°</option>
+            <option value5="fundamental1">4°</option>
+            <option value6="fundamental1">5°</option>
+          </select>
+        </label>
+        <label>
+          <select className={style.button} name="etapa" onChange={handleFilterChange} value={filter.etapa}>
+            <option value="">Ensino Fundamental 2</option>
+            <option value2="fundamental2">6º</option>
+            <option value3="fundamental2">7°A</option>
+            <option value4="fundamental2">7°B</option>
+            <option value5="fundamental2">8°A</option>
+            <option value6="fundamental2">8°B</option>
+            <option value7="fundamental2">9ºA</option>
+            <option value8="fundamental2">9ºB</option>
+          </select>
+        </label>
+        <label>
+          <select className={style.button} name="ano" onChange={handleFilterChange} value={filter.ano}>
+            <option value="">Ensino Medio</option>
+            <option value2="medio">1ºA</option>
+            <option value3="medio">1ºB</option>
+            <option value4="medio">2º</option>
+            <option value5="medio">3º</option>
+          </select>
+        </label>
 
+        <label>
+          <select className={style.button} name="ano" onChange={handleFilterChange} value={filter.ano}>
+            <option value="">Etapa</option>
+            <option value2="etapa">1º</option>
+            <option value3="etapa">2º</option>
+            <option value4="etapa">3º</option>
+          </select>
+        </label>
 
-      <Header />
-
-      <div className={styles.body}>
+        <div className={style.ano}>
         
-        <h1 className={styles.titulo}>AVALIAÇÕES EXTERNAS</h1>
+        <label>Ano</label>
+        
+          <input className={style.input} type="number"/>
+        
+        </div>
 
-        <div className={styles.button}>
-
-            <label>
-
-              <select className={styles.teste} name="ensino" onChange={handleFilterChange} value={filter.ensino}>
-                <option value=""> Ensino Fundamental 1</option>
-                <option value1="fundamental1">3ºano</option>
-                <option value2="fundamental1">4ºano</option>
-
-                <option value3="fundamental1">5ºano</option>
-
-              </select>
-            </label>
-            
-            <label>
-
-
-
-              <select className={styles.teste} name="ensino" onChange={handleFilterChange} value={filter.ensino}>
-                <option value="">Ensino Fundamental 2</option>
-                <option value1="fundamental2">6ºano</option>
-                <option value2="fundamental2">7ºano A</option>
-                <option value3="fundamental2">7ºano B</option>
-                <option value4="fundamental2">8ºano A</option>
-                <option value5="fundamental2">8ºano B</option>
-                <option value6="fundamental2">9ºano A</option>
-                <option value7="fundamental2">9ºano B</option>
-
-              </select>
-
-
-            </label>
-
-            <label>
-              <select className={styles.teste} name="ensino" onChange={handleFilterChange} value={filter.ensino}>
-                <option value="">Ensino Médio</option>
-                <option value1="fundamental2">1ºano A </option>
-                <option value1="fundamental2">1ºano B </option>
-                <option value2="fundamental2">2ºano </option>
-                <option value3="fundamental2">3ºano </option>
-              </select>
-            </label>
-
-            <label>
-              <select className={styles.teste} name="etapa" onChange={handleFilterChange} value={filter.etapa}>
-                <option value="">Etapa</option>
-
-                <option value1="etapa">1 etapa </option>
-                <option value2="etapa">2 etapa </option>
-                <option value3="etapa">3 etapa </option>
-              </select>
-            </label> 
-
-            <select className={styles.teste} name="Tipo de prova" onChange={handleFilterChange} value={filter.prova}>
-              <option value="">Tipo de prova</option>
-              <option value1="prova 1">Desbrava Enem</option>
-              <option value2="prova 2">Saresp</option>
-              <option value3="prova 3">Canguru</option>
-              </select>
-
-
-          <div className={styles.teste}>
-            <label>
-              Ano:
-              <input className={styles.input} type="number" name="ano" />
-            </label>
-            
-            </div>
-
+        <button className={style.button} onClick={handleFilter}>Filtrar</button>
        
-          <button className={styles.teste} onClick={handleFilter}>Filtrar</button>
-         
         </div>
+       
         
-        </div>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th className={styles.th}>Aluno</th>
-              <th className={styles.th}>RM</th>
+        <h1 className={style.text}>Curso Técnico</h1>
+        
+      
 
+      <div className="info">
+      <table className={style.table}>
+        <thead>
+          <tr>
+            <th className={style.th}>Aluno</th>
+            <th className={style.th}>RM</th>
+            <th className={style.th}>ETAPA</th>
+            <th className={style.th}>ANO</th>
+            <th className={style.th}>NOTA</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredStudents.map((student, index) => (
+            <tr key={index}>
+              <td className={style.td}>{student.aluno}</td>
+              <td className={style.td}>{student.rm}</td>
+              <td className={style.td}>{student.etapa}</td>
+              <td className={style.td}>{student.ano}</td>
+              <td className={style.td}>{student.nota}</td>
             </tr>
-          </thead>
-          <tbody>
-            {filteredStudents.map((student, index) => (
-              <tr key={index}>
-                <td className={styles.td}>{student.aluno}</td>
-                <td className={styles.td}>{student.rm}</td>
+          ))}
+        </tbody>
+      </table> 
 
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className={style.botao}>
+        <button className={style.button} onClick={() => alert('Ação não implementada!')}>Editar</button>
+        <button className={style.button} onClick={() => alert('Ação não implementada!')}>Salvar</button>
+      </div>
 
+      </div>
 
-
-      <Footer />
+            <Footer/>
 
     </>
-
+    
   );
 
 };
+
 export default Home;
