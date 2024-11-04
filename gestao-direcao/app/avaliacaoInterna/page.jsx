97a02795e3aa6d9dde740f2a259643a6899183ba
Nathizinhaa <Nathaliapasquotto@gmail.com>
2024-11-04 10:09:45 -0300

    oficial

--- gestao-direcao/app/avaliacaoInterna/page.jsx
+++ gestao-direcao/app/avaliacaoInterna/page.jsx
@@ -5,72 +5,47 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 const studentsData = [
-  { aluno: 'João', rm: '123', etapa: '3', ano: '2023', nota: '8.5', ensino: '1°A' },
-  { aluno: 'Maria', rm: '456', etapa: '4', ano: '2024', nota: '9.0', ensino: '1°B' },
-  { aluno: 'Pedro', rm: '789', etapa: '5', ano: '2025', nota: '7.0', ensino: '2°' },
-  { aluno: 'Ana', rm: '101', etapa: '1', ano: '2026', nota: '6.5', ensino: '3°EM' },
-  { aluno: 'Lucas', rm: '202', etapa: '2', ano: '2027', nota: '9.5', ensino: '4°' },
 ];
 
 const Home = () => {
   const [ensinoTurma, setEnsinoTurma] = useState(''); // add state for each select
   const [etapa, setEtapa] = useState('');
   const [ano, setAno] = useState('');
-  const descricaoRef = useRef(null);
-
-
-  useEffect(() => {
-    if (descricaoRef.current) {
-      descricaoRef.current.innerHTML = '';
-    }
-  }, []);
-
-
-
 
   const getFilter = async () => {
     if (ensinoTurma && etapa && ano) {
-      const url = `http://localhost:3001/avaliasesi/${etapa}/${ensinoTurma}/${ano}`;  //http://localhost:3001/avaliasesi/1S/3%25E.M/2024
+      const url = `http://localhost:3001/${ensinoTurma}/${etapa}/${ano};`  //http://localhost:3001/avaliasesi/1S/3%25E.M/2024
       console.log(`Constructed URL: ${url}`);
-      console.log('Current state:', etapa, ensinoTurma, ano);
+      console.log('Current state:', ensinoTurma, etapa, ano);
+      
 
+      
 
       try {
+        debugger
+
         const response = await fetch(url);
         console.log(response);
         const resData = await response.json();
         console.log(resData);
 
-
         // Create a table element
         // Create a table element
-
-
         const table = document.createElement('table');
-        table.className = style.table;
-        // Verifique se a classe foi adicionada
-        // add a border to the table
-
+        table.border = '1'; // add a border to the table
 
         // Create a header row
         const headerRow = table.insertRow(0);
         const headers = Object.keys(resData[0]);
         headers.forEach((header, index) => {
           const th = document.createElement('th');
-          if (header === 'rm') {
-            th.innerHTML = 'RM';
-          } else if (header === 'Turma') {
-            th.innerHTML = 'Turma';
-          } else if (header === 'PorcentagemAcertoIngles') {
-            th.innerHTML = 'Porcentagem Acerto Inglês';
-          } else if (header === 'Ebep' || header === 'ComDeficiencia') {
-            th.innerHTML = header.replace('Ebep', 'E.B.E.P').replace('ComDeficiencia', 'Com Deficiência');
+          if (header === 'NomeAluno') {
+              th.innerHTML = 'Nome do Aluno';
           } else {
-            th.innerHTML = header.replace('1S-', '1ª Etapa ').replace('2S-', '2ª Etapa ').replace('3S-', '3ª Etapa ').replace('CH', 'Ciências Humanas').replace('CN', 'Ciências Naturais').replace('LI', 'Língua Inglesa').replace('LP', 'Língua Portuguesa').replace('MAT', 'Matemática');
+              th.innerHTML = header.replace('1Etapa', '1ª Etapa ').replace('AR', 'Artes').replace('CCE', 'CCE').replace('CH', 'Ciências Humanas').replace('CN', 'Ciências Naturais').replace('EF', 'Educação Física').replace('LI', 'Língua Inglesa').replace('LP', 'Língua Portuguesa').replace('MAT', 'Matemática').replace('PF', 'Pensamento Filosófico').replace('PR', 'Programação').replace('PSC', 'PSC').replace('ROB', 'Robótica');
           }
           headerRow.appendChild(th);
         });
-
 
         // Create rows for each data item
         resData.forEach((item) => {
@@ -87,7 +62,6 @@ const Home = () => {
           });
         });
 
-
         // Add the table to the #descricao div
         document.getElementById("descricao").innerHTML = '';
         document.getElementById("descricao").appendChild(table);
@@ -99,25 +73,21 @@ const Home = () => {
     }
   }
 
-
   // add event handlers for each select
   const handleEnsinoTurmaChange = (e) => {
     console.log('etapa changed:', e.target.value);
     setEnsinoTurma(e.target.value);
   }
 
-
   const handleEtapaChange = (e) => {
     console.log('ensinoTurma changed:', e.target.value);
     setEtapa(e.target.value);
   }
 
-
   const handleAnoChange = (e) => {
     console.log('ano changed:', e.target.value);
     setAno(e.target.value);
   }
-
 
   const [filter, setFilter] = useState({
     ensino: '',
@@ -127,8 +97,6 @@ const Home = () => {
 
   const [filteredStudents, setFilteredStudents] = useState(studentsData);
   const [availableEtapas, setAvailableEtapas] = useState([]);
-
- 
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
@@ -167,9 +135,6 @@ const Home = () => {
     }
   };
 
-
-
-
   const handleFilter = () => {
     const newFilteredStudents = studentsData.filter((student) =>
       (filter.ensino ? student.ensino === filter.ensino : true) &&
@@ -184,67 +149,47 @@ const Home = () => {
       <Header />
       <div className={style.filtro}>
         <label>
-          <select className={style.button} name="ensino" value={ensinoTurma} onChange={handleEnsinoTurmaChange}>
-            <option value="">EF1</option>
-            <option value="1%25E.F">1º Ano</option>
-            <option value="2%25E.F">2º Ano</option>
-            <option value="3%25E.F">3º Ano</option>
-            <option value="4%25E.F">4º Ano</option>
-            <option value="5%25E.F">5º Ano</option >
-          </select>
+        <select className={style.button} value={ensinoTurma} onChange={handleEnsinoTurmaChange}>
+                <option value="">EF1</option>
+                <option value="notasEF1/1%25E.F">1º Ano</option>
+                <option value="notasEF1/2%25E.F">2º Ano</option>
+                <option value="notasEF1/3%25E.F">3º Ano</option>
+                <option value="notasEF1/4%25E.F">4º Ano</option>
+                <option value="notasEF1/5%25E.F">5º Ano</option>
+            </select>
 
 
-        </label>
-
-
-
-
-        <label>
           <select className={style.button} name="ensino" value={ensinoTurma} onChange={handleEnsinoTurmaChange}>
             <option value="">EF2</option>
-            <option value="6%25A%25">6º Ano A</option>
-            <option value="6%25B%25">6º Ano B</option>
-            <option value="7%25A%25">7º Ano A</option>
-            <option value="7%25B%25">7º Ano B</option>
-            <option value="8%25A%25">8º Ano A</option>
-            <option value="8%25B%25">8º Ano B</option>
-            <option value="9%25A%25">9º Ano A</option>
-            <option value="9%25B%25">9º Ano B</option>
+            <option value="notasEF2/6%25A%25">6º Ano A</option>
+            <option value="notasEF2/6%25B%25">6º Ano B</option>
+            <option value="notasEF2/7%25A%25">7º Ano A</option>
+            <option value="notasEF2/7%25B%25">7º Ano B</option>
+            <option value="notasEF2/8%25A%25">8º Ano A</option>
+            <option value="notasEF2/8%25B%25">8º Ano B</option>
+            <option value="notasEF2/9%25A%25">9º Ano A</option>
+            <option value="notasEF2/9%25B%25">9º Ano B</option>
           </select>
 
           <select className={style.button} name="ensino" value={ensinoTurma} onChange={handleEnsinoTurmaChange}>
             <option value="">EM</option>
-            <option value="1%25A%25">1º Ano A</option>
-            <option value="1%25B%25">1º Ano B</option>
-            <option value="2%25E.M">2º Ano</option>
-            <option value="3%25E.M">3º Ano</option>
+            <option value="notasEM/1%25A%25">1º Ano A</option>
+            <option value="notasEM/1%25B%25">1º Ano B</option>
+            <option value="notasEM/2%25E.M">2º Ano</option>
+            <option value="notasEM/3%25E.M">3º Ano</option>
 
           </select>
 
 
         </label>
-
-
-        <label>
-          
-          <select className={style.button} name="ensino" value={ensinoTurma} onChange={handleEnsinoTurmaChange}>
-            <option value="">EM</option>
-            <option value="1%25A%25">1º Ano A</option>
-            <option value="1%25B%25">1º Ano B</option>
-            <option value="2%25E.M">2º Ano</option>
-            <option value="3%25E.M">3º Ano</option>
-          </select>
-        </label>
-
-
 
 
         <label>
           <select className={style.button} name="etapa" onChange={handleEtapaChange} value={etapa}>
-            <option value="">Etapa</option>
-            <option value="1S">1</option>
-            <option value="2S">2</option>
-            <option value="3S">3</option>
+            <option value="">Selecione</option>
+            <option value="%251etapa">1</option>
+            <option value="%252etapa">2</option>
+            <option value="%253etapa">3</option>
           </select>
         </label>
 
@@ -297,32 +242,8 @@ const Home = () => {
           <button className={style.button} onClick={() => alert('Ação não implementada!')}>Editar</button>
           <button className={style.button} onClick={() => alert('Ação não implementada!')}>Salvar</button>
         </div>
-
-
-
-
-
-        <button className={style.button} onClick={getFilter} disabled={!ensinoTurma || !etapa || !ano}>Filtrar</button>
-
-
-
-
       </div>
-
-
-      <h1 className={style.text}>Avaliação Interna</h1>
-
-
-     
-        <div className={style.table} id='descricao' ref={descricaoRef} />
-       
-              <div className={style.footer}>
-
-
       <Footer />
-      </div>
-
-
     </>
   );
 };
